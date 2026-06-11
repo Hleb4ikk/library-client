a4471f7e54e8a9375816c1ff7a6885263f124122
ulyana <uzh0976@gmail.com>
2026-06-11 12:22:10 +0300

    add bcrypt password hashing utilities

--- server/src/appConfig.ts
+++ server/src/appConfig.ts
@@ -8,10 +8,31 @@ if (!jwtSecret) {
   throw new Error('ACCESS_TOKEN_SECRET environment variable is not defined');
 }
 
+const DEFAULT_BCRYPT_SALT_ROUNDS = 10;
+
+function parseBcryptSaltRounds(): number {
+  const value = process.env.BCRYPT_SALT_ROUNDS;
+
+  if (!value) {
+    return DEFAULT_BCRYPT_SALT_ROUNDS;
+  }
+
+  const saltRounds = Number(value);
+
+  if (!Number.isInteger(saltRounds) || saltRounds < 4 || saltRounds > 31) {
+    throw new Error('BCRYPT_SALT_ROUNDS must be an integer between 4 and 31');
+  }
+
+  return saltRounds;
+}
+
 export const appConfig = {
   port: Number(process.env.APP_PORT),
   jwt: {
     secret: jwtSecret,
     accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN ?? '15m',
+  },
+  bcrypt: {
+    saltRounds: parseBcryptSaltRounds(),
   },
 };
