f54ca3337f0afc1d9d2c4d988d00a6077effb308
ilyas <ilyakoval2006@gmail.com>
2026-06-12 14:01:33 +0300

    feature/supabase-connection and init orm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,22 @@
 import express from 'express';
 
 import { appConfig } from '@/appConfig.js';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import 'dotenv/config';
 
 const app = express();
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("❌ ОШИБКА: Переменная DATABASE_URL не найдена в файле .env");
+}
+
+const client = postgres(connectionString, { prepare: false });
+
+export const db = drizzle(client);
+
+console.log("✅ Drizzle инициализирован");
 
 app.listen(appConfig.port, (error) => {
   if (!error) {
