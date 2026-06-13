715a9e56a57cf9ef4773c230c658169412c907a2
Глеб Нюнько <nunkogleb348@gmail.com>
2026-06-13 13:35:32 +0300

    added drizzle-orm using, added drizzle-kit migration system

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,10 @@
 import express from "express";
 
-<<<<<<< HEAD
-import { appConfig } from '@/appConfig.js';
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import 'dotenv/config';
-=======
 import { appConfig } from "@/appConfig.js";
+import "dotenv/config";
 import { errorHandler } from "./middleware/error.handler.js";
->>>>>>> develop
 
 const app = express();
-const connectionString = process.env.DATABASE_URL;
-
-if (!connectionString) {
-  throw new Error("❌ ОШИБКА: Переменная DATABASE_URL не найдена в файле .env");
-}
-
-const client = postgres(connectionString, { prepare: false });
-
-export const db = drizzle(client);
-
-console.log("✅ Drizzle инициализирован");
 
 app.use(errorHandler);
 
