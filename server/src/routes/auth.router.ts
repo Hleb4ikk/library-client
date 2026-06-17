639d65963c5d9a805e7002527054af7c1a792650
ulyana <uzh0976@gmail.com>
2026-06-17 13:39:43 +0300

    add login endpoint with validation

--- server/src/routes/auth.router.ts
+++ server/src/routes/auth.router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { register } from "@/controllers/auth.controller.js";
+import { login, register } from "@/controllers/auth.controller.js";
 import { validationMiddleware } from "@/middleware/validation.middleware.js";
 import { registerSchema } from "@/schemas/register.schema.js";
+import { loginSchema } from "@/schemas/login.schema.js";
 
 const authRouter = Router();
 
 authRouter.post('/register', validationMiddleware(registerSchema), register);
+authRouter.post('/login', validationMiddleware(loginSchema), login);
 
 export default authRouter;
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,9 +1,37 @@
-import { createUser } from "@/repositories/user.repository.js";
-import { hashPassword } from "@/utils/password.utils.js";
+import ApiError from "@/classes/ApiError.js";
+import { createUser, findUserByUsername } from "@/repositories/user.repository.js";
+import type { AccessTokenPayload } from "@/schemas/access-token-payload.schema.js";
+import { hashPassword, comparePassword } from "@/utils/password.utils.js";
+import { generateAccessToken } from "@/utils/token.utils.js";
 
 export async function registerUser(username: string, password: string): Promise<{id: number, username: string} | undefined> {
     const passwordHash = await hashPassword(password);
     const user = await createUser({username: username, passwordHash: passwordHash});
 
     return user;
+}
+
+export async function authenticateUser(username: string, password: string): Promise<{ 
+    user: {id: number, username: string}, 
+    token: string 
+}> {
+    const user = await findUserByUsername(username);
+    if (!user) {
+        throw new ApiError(401, 'Неверное имя пользователя или пароль');
+    }
+
+    const isPasswordValid = await comparePassword(password, user.passwordHash);
+    if (!isPasswordValid) {
+        throw new ApiError(401, 'Неверное имя пользователя или пароль');
+    }
+
+    const token = generateAccessToken(user.id);
+
+    return {
+        user: {
+            id: user.id,
+            username: user.username
+        },
+        token: token
+    }
 }
