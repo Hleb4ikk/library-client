42b4affe637d6895e836a79972995840475c80ee
beecatt <uliastas524@gmail.com>
2026-06-26 14:24:43 +0300

    feat: add comments page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import AuthPage from "./pages/AuthPage";
 import BookDetailsPage from "./pages/book-details-page";
+import CommentsPage from "./pages/comments-page";
 import HomePage from "./pages/home-page";
 import ProfilePage from "./pages/profile-page";
 import ReadingListPage from "./pages/reading-list-page";
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/books/:bookId" element={<BookDetailsPage />} />
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/comments" element={<CommentsPage />} />
         <Route path="/reading-list" element={<ReadingListPage />} />
       </Routes>
     </BrowserRouter>
