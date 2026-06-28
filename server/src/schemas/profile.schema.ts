f5b471e5973689187774ba545cdcd484f420ba84
Глеб Нюнько <nunkogleb348@gmail.com>
2026-06-28 03:04:45 +0300

    resolved conflict

--- server/src/schemas/profile.schema.ts
+++ server/src/schemas/profile.schema.ts
@@ -1,38 +1,54 @@
 import z from "zod";
 
 export const changeLoginBodySchema = z.object({
-    new_username: z.string().min(3)
+  new_username: z.string().min(3),
 });
 
 export const changePasswordBodySchema = z.object({
-    current_password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/),
-    new_password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/)
+  current_password: z
+    .string()
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+    ),
+  new_password: z
+    .string()
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+    ),
 });
 
-export const getLikesQuerySchema = z.object({
-    page: z
-        .string()
-        .optional()
-        .transform((val) => (val ? parseInt(val, 10) : 1))
-        .refine((val) => !isNaN(val) && val > 0, { message: "Номер страницы должен быть положительным числом" }),
-    limit: z
-        .string()
-        .optional()
-        .transform((val) => (val ? parseInt(val, 10) : 3))
-        .refine((val) => !isNaN(val) && val > 0, { message: "Лимит должен быть положительным числом" }),
+export const paginationQuerySchema = z.object({
+  page: z
+    .string()
+    .optional()
+    .transform((val) => (val ? parseInt(val, 10) : 1))
+    .refine((val) => !isNaN(val) && val > 0, {
+      message: "Номер страницы должен быть положительным числом",
+    }),
+  limit: z
+    .string()
+    .optional()
+    .transform((val) => (val ? parseInt(val, 10) : 3))
+    .refine((val) => !isNaN(val) && val > 0, {
+      message: "Лимит должен быть положительным числом",
+    }),
 });
 
 export const searchMyBooksQuerySchema = z.object({
-    q: z.string().optional(),
-    type: z.enum(["likes", "reading_list", "all"]).default("all"),
-    page: z
-        .string()
-        .optional()
-        .transform((val) => (val ? parseInt(val, 10) : 1))
-        .refine((val) => !isNaN(val) && val > 0, { message: "Номер страницы должен быть положительным числом" }),
-    limit: z
-        .string()
-        .optional()
-        .transform((val) => (val ? parseInt(val, 10) : 10))
-        .refine((val) => !isNaN(val) && val > 0, { message: "Лимит должен быть положительным числом" }),
+  q: z.string().optional(),
+  type: z.enum(["likes", "reading_list", "all"]).default("all"),
+  page: z
+    .string()
+    .optional()
+    .transform((val) => (val ? parseInt(val, 10) : 1))
+    .refine((val) => !isNaN(val) && val > 0, {
+      message: "Номер страницы должен быть положительным числом",
+    }),
+  limit: z
+    .string()
+    .optional()
+    .transform((val) => (val ? parseInt(val, 10) : 10))
+    .refine((val) => !isNaN(val) && val > 0, {
+      message: "Лимит должен быть положительным числом",
+    }),
 });
